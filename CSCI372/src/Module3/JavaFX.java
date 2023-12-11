1003468a13778d2ee74ddbfba83cecaba78d8f1c
Long Chang <Longleechang@gmail.com>
2023-12-10 18:14:21 -0800

    created Module 3, added JavaFX libraries

---
+++ CSCI372/src/Module3/JavaFX.java
@@ -0,0 +1,21 @@
+package Module3;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.TextField;
+
+public class JavaFX extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
