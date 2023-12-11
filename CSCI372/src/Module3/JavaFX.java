5adb91a6ee49acc45b4a14a98a8ca33262b215cb
Long Chang <Longleechang@gmail.com>
2023-12-10 21:42:19 -0800

    added changing background on start

--- CSCI372/src/Module3/JavaFX.java
+++ CSCI372/src/Module3/JavaFX.java
@@ -1,16 +1,18 @@
 package Module3;
+import java.awt.Color;
+import javafx.event.ActionEvent;
+
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import java.util.Random;
 
-public class JavaFX extends Application{
+public class JavaFX extends Application implements EventHandler<ActionEvent>{
 
 	Button timeButton;
 	Button logButton;
@@ -22,6 +24,20 @@ public class JavaFX extends Application{
 		launch(args);
 	}
 
+	public int random() {
+		Random rand = new Random();
+		int x = 0;
+		while(x<10) {
+			x = rand.nextInt(99);
+		}
+		return x;
+	}
+	
+	public String randomColor() {
+		String x = random() + "";
+		String y = random() + "";
+		return "-fx-background-color: #" + x + "cc" + y; 
+	}
 	
 	@Override
 	public void start(Stage stage1) throws Exception {
@@ -32,6 +48,7 @@ public class JavaFX extends Application{
 		timeButton = new Button("Show Time");
 		logButton = new Button("Log Time");
 		colorButton = new Button("Change background");
+		colorButton.setOnAction(this);
 		exitButton = new Button("Exit");
 		
 		VBox vbox = new VBox(4);
@@ -43,9 +60,23 @@ public class JavaFX extends Application{
 		vbox.getChildren().add(exitButton);
 		
 		Scene scene = new Scene(vbox, 300, 300);
+		vbox.setStyle(randomColor());
 		
 		stage1.setScene(scene);
 		stage1.show();
+		
 	}
+
+
+	@Override
+	public void handle(ActionEvent event) {
+		if(event.getSource()==colorButton) {
+			randomColor();
+			
+		}
+		
+	}
+
+
 
 }
