752f4c567a2d5f0de0a3cf24360d202800f3766a
Long Chang <Longleechang@gmail.com>
2024-01-07 17:12:43 -0800

    -made SumRecursion it's own class with main for driver. -added comments for SumRecursion to better identify what is going on

---
+++ CSCI372/src/Recursion/Driver.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		System.out.println("The total sum is: " + sumRecursion.recursionSum(5));
+
+	}
+	
+	
+}
--- CSCI372/src/Recursion/sumRecursion.java
+++ CSCI372/src/Recursion/sumRecursion.java
@@ -2,26 +2,29 @@ package Recursion;
 
 import java.util.Scanner;
 
-public class SumRecursion {
-
-	public static void main(String[] args) {
-		
-		System.out.println("The total sum is: " + recursionSum(5));
-
-	}
-	
+public class sumRecursion {
 
 	public static int recursionSum(int i) {
+		
+		//i>0 is condition for return
 		if(i > 0) {
+			
+			//use try method in case user does not input number
 			try (Scanner myObj = new Scanner(System.in)) {
+				
+				//statement to get input from user
 				System.out.println("Enter " + i +"th number:"); 
 				int x = myObj.nextInt();   
 				   
+				//confirm user input
 				System.out.println("choice is: " + x);
 				
+				//return method
 				return x + recursionSum(i-1);
 			}
 		}
+		
+		//return method to ensure recursion ends
 		return 0;
 	}
 	
